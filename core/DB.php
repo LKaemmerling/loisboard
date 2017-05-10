28d68deb78042d26270e048fe7b05fab0be9ddaa
Sascha Loishandl <sascha.loishandl@hotmail.com>
2017-05-10 11:42:31 +0200

    PDO (PDB) Klasse hinzugefügt
    Klasse für den umstieg auf PDO hinzugefügt.

--- core/DB.php
+++ core/DB.php
@@ -168,4 +168,225 @@ class DB {
         
     }
 }
+
+class PDB 
+{
+    public static $db = null; 
+
+    /**
+    * Verbindung herstellen
+    *
+    * Stellt die Verbindung zur MySql Datenbank her. Die Verbindung wird in der public static $db Variable gespeichert. 
+    * Sendet eine Fehlermeldung wenn die Verbindung nicht erfolgreich hergestellt werden konnte. 
+    * 
+    * @author s-l 
+    * @version 0.1.0 
+    * @return bool 
+    */
+    public static function init($dbHost, $dbUser, $dbPassword, $dbDatabase)  
+    {
+        try {
+            self::$db = new \PDO('mysql:host='.$dbHost.';dbname='.$dbDatabase.';charset=utf8mb4', $dbUser, $dbPassword);
+        } catch(PDOException $ex) {
+            echo "<strong>DB::".__FUNCTION__." Error:</strong> Es konnte keine Verbindung hergestellt werden!"; 
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__." Error:</strong> Es konnte keine Verbindung hergestellt werden!"; 
+            return false; 
+        }
+        self::$db->query("SET NAMES utf-8"); 
+        return true; 
+    }
+
+    /**
+    * Verbindung testen
+    *
+    * Versucht eine Verbindung zur MySql Datenbank herzustellen. Die Verbindung wird nicht gehalten. 
+    *
+    * @author s-l 
+    * @version 0.0.1 
+    * @return bool 
+    */
+    public static function testConnection($dbHost, $dbUser, $dbPassword, $dbDatabase) 
+    {
+        try {
+            $db = new \PDO('mysql:host='.$dbHost.';dbname='.$dbDatabase.';charset=utf8mb4', $dbUser, $dbPassword);
+        } catch(PDOException $ex) {
+            return false; 
+        }
+        return true; 
+    }
+
+    /**
+    * Query ausführen
+    *
+    * Führt eine Query direkt aus. 
+    * Optional: Die Variable $params kann Optional mit Parametern befüllt werden.
+    *
+    * @author s-l 
+    * @version 0.1.3
+    * @return array
+    */
+    public static function query($sql, $params=null) 
+    {
+        if(self::$db == null) {
+            echo "<strong>DB::".__FUNCTION__." Error:</strong> Es wurde keine Verbindung zur Datenbank gefunden.";
+            return; 
+        }
+        $result = array(); 
+        try {
+            $state = self::$db->prepare($sql); 
+            if($params == null)
+                $state->execute(); 
+            else
+                $state->execute($params); 
+            $result = $state->fetchAll(); 
+        } catch(PDOException $ex) {
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__." Error:</strong> " . $ex; 
+        }
+        return $result; 
+    }
+
+    /**
+    * INSERT
+    *
+    * Wird verwendet um Daten in die Datenbank einzutragen. Dabei wird die Tabelle und ein Array mit Key => Value eingetragen. 
+    *
+    * @param $table(string) Die Datenbank-Tabelle in die etwas eingetragen werden soll
+    * @param $datas(array) Die Daten die eingetragen werden sollen - im Style Key => Value 
+    * @author s-l 
+    * @version 0.0.6 
+    */
+    public static function insert($table, $datas) 
+    {
+        $sql = "INSERT INTO $table";
+        $keys = ""; 
+        $values = ""; 
+        $params = array(); 
+        foreach($datas as $key => $value) {
+            $keys .= $key.",";
+            $values .= "?,";  
+            $params[] = $value; 
+        }
+        $keys = rtrim($keys, ","); 
+        $values = rtrim($values, ","); 
+        $sql .= " ($keys) VALUES ($values)";
+        try 
+        {
+            $state = self::$db->prepare($sql); 
+            $state->execute($params); 
+        } 
+        catch(PDOException $ex) 
+        {
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__. "Error:</strong> ".$ex;
+        }
+    }
+
+    /**
+    * UPDATE
+    *
+    * Wird verwendet um einen Tabelleneintrag (von dem die ID bekannt ist) upzudaten. 
+    *
+    * @param $table(string) Die Datenbank-Tabelle
+    * @param $id(int) Die Row-ID die geupdated werden soll 
+    * @param $data(string) Alle Felder die geupdated werden sollen 
+    * @param $params(array) Die Parameter die in $data eingesetzt werden sollen 
+    * @author s-l 
+    * @version 0.0.2 
+    */
+    public static function update($table, $id, $data, $params=array()) 
+    {
+        $sql = "UPDATE $table SET ".$data." WHERE id=?";
+        $params[] = $id; 
+        try {
+            $state = self::$db->prepare($sql); 
+            $state->execute($params); 
+        } 
+        catch(PDOException $ex) {
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__. "Error:</strong> ".$ex;
+        }
+    }
+
+    /**
+    * SELECT
+    * 
+    * Wird verwendet um Daten aus einer Tabelle auszulesen. Kann verschiedene Parameter beinhalten um die Suche zu verfeinern. 
+    *
+    * @param $table(string) Die Datenbank-Tabelle
+    * @param $fields(string) Die Felder die zurückgegeben werden
+    * @param $where(string) Die WHERE Angaben (z.B.: "id=? AND ground=?")
+    * @param $order(string) Die Order Angaben (z.B.: "lastCheck DESC, id DESC")
+    * @param $limit(string) Die Limit Angaben (z.B.: "5, 24")
+    * @param $params(array) Die Parameter (für gewöhnlich bei $where verwendet) Inhalte die Escaped werden sollen
+    * @author s-l 
+    * @version 0.0.9 
+    */
+    public static function select($table, $fields="*", $where=null, $order=null, $limit=null, $params=null)
+    {
+        $sql = "SELECT $fields FROM $table";
+        if($where != null) 
+        {
+            $sql .= " WHERE " . $where;
+        }
+        if($order != null) 
+        {
+            $sql .= " ORDER BY " . $order;
+        }
+        if($limit != null) 
+        {
+            $sql .= " LIMIT " . $limit;
+        }
+        try {
+            $state = self::$db->prepare($sql); 
+            if($params != null) 
+                $state->execute($params); 
+            else
+                $state->execute(); 
+            $rst = $state->fetchAll(); 
+            return $rst; 
+        }
+        catch(PDOException $ex) {
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__. "Error:</strong> ".$ex;
+        }
+    }
+
+    /**
+    * DELETE
+    *
+    * Wird verwendet um einen Eintrag aus einer Tabelle zu löschen.
+    *
+    * @param $table(string) Die Datenbank-Tabelle
+    * @param $where(string) Die WHERE Angaben (z.B.: "id=?")
+    * @param $params(array) Die Parameter die Escaped werden sollen 
+    * @author s-l 
+    * @version 0.0.3
+    */
+    public static function delete($table, $where, $params=null) 
+    {
+        $sql = "DELETE FROM $table WHERE " . $where;
+        try {
+            $state = self::$db->prepare($sql); 
+            if($params != null)
+                $state->execute($params); 
+            else 
+                $state->execute(); 
+        }
+        catch(PDOException $ex) {
+            $_SESSION["errors"][] = "<strong>DB::".__FUNCTION__. "Error:</strong> ".$ex;
+        }
+    }
+
+    /**
+    * Insert ID 
+    *
+    * Lest die letzte eingesetzte ID aus. 
+    *
+    * @author s-l 
+    * @version 0.0.1 
+    */
+    public static function insertID() 
+    {
+        return self::$db->lastInsertId(); 
+    }
+
+
+}
 ?>
